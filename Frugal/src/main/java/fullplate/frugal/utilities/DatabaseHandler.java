07e3755d1e24fd57096d1553fa6f9496b9635a73
Michael Harmer <michaelharmer.au@gmail.com>
2014-05-08 10:55:30 +1000

    [ADD] SQLiteOpenHelper implementation for entries table

--- Frugal/src/main/java/fullplate/frugal/utilities/DatabaseHandler.java
+++ Frugal/src/main/java/fullplate/frugal/utilities/DatabaseHandler.java
@@ -1,4 +1,43 @@
 package fullplate.frugal.utilities;
 
-public class DatabaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    // todo: see 12. performance
+
+    private static final String TABLE_ENTRIES = "entries";
+    private static final String COLUMN_TIMESTAMP = "_timestamp";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_AMOUNT = "amount";
+
+    private static final String DATABASE_NAME = "entries.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE = "create table " + "("
+            + COLUMN_TIMESTAMP + "integer primary key, "
+            + COLUMN_DESCRIPTION + "text"
+            + COLUMN_AMOUNT + "integer not null);";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        Log.w(DatabaseHandler.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_ENTRIES);
+        onCreate(database);
+    }
+
 }

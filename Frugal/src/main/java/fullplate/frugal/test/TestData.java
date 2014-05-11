fdb71399fea61bf26f36988181819bd289aef5fe
Michael Harmer <michaelharmer.au@gmail.com>
2014-05-11 18:44:18 +1000

    Juicy test data for generating screenshots

--- Frugal/src/main/java/fullplate/frugal/test/TestData.java
+++ Frugal/src/main/java/fullplate/frugal/test/TestData.java
@@ -1,6 +1,7 @@
 package fullplate.frugal.test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import fullplate.frugal.domain.Entry;
 import fullplate.frugal.domain.PeriodicEntry;
@@ -11,17 +12,31 @@ public class TestData {
     public static ArrayList<Entry> generateTestEntries() {
         long day = 86400000;
 
+        HashMap<String, Integer> testData = new HashMap<>();
+        testData.put("Breakfast", 6);
+        testData.put("Lunch", 10);
+        testData.put("Dinner", 25);
+        testData.put("Drinks", 15);
+        testData.put("Clothes", 50);
+        testData.put("Charity Donation", 20);
+        testData.put("Morning Coffee", 3);
+        testData.put("Electricity Bill", 200);
+
+        String[] keys = testData.keySet().toArray(new String[testData.size()]);
+
         ArrayList<Entry> entries = new ArrayList<>();
 
-        for (int i = 0; i < 40; i++) {
-            long time = generateTestStartTime()+(i*day);
+        for (int i = 0; i < 500; i++) {
+            long time = generateTestStartTime()+(i*day/2);
             if (time < System.currentTimeMillis()) {
-                entries.add(new SingleEntry(generateTestStartTime()+(i*day), i, "Description "+Integer.toString(i)));
+                String item = keys[i % testData.size()];
+                int value = testData.get(item);
+                entries.add(new SingleEntry(time, value, item));
             }
         }
 
-        entries.add(new PeriodicEntry(10, "P1"));
-        entries.add(new PeriodicEntry(500, "P2"));
+        entries.add(new PeriodicEntry(200, "Rent"));
+        entries.add(new PeriodicEntry(50, "Groceries"));
 
         return entries;
     }
